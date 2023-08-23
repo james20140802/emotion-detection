f9e1e5e59c0cf8bcb2370fa10030144973bbd666
SangChu04 <drchasekim@gmail.com>
2023-08-23 16:46:03 +0900

    :bug: Fix data_path

--- src/data/load/load_text_emotion.py
+++ src/data/load/load_text_emotion.py
@@ -7,12 +7,12 @@ import os
 import pandas as pd
 
 
-def load():
+def loada():
     """
     data/raw 폴더 속 Text_Emotion/train.csv 의 text data를 로드함.
     """
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    data_path = os.path.join(current_dir, "../../data/raw/Text_Emotion/train.csv")
+    data_path = os.path.join(current_dir, "../../../data/raw/Text_Emotion/train.csv")
 
     dataframe = pd.read_csv(data_path)
 
--- src/data/load/load_tweet_emotions.py
+++ src/data/load/load_tweet_emotions.py
@@ -12,7 +12,7 @@ def load():
     data/raw 폴더 속 tweet_emotions.csv 의 text data를 로드함.
     """
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    data_path = os.path.join(current_dir, "../../data/raw/tweet_emotions.csv")
+    data_path = os.path.join(current_dir, "../../../data/raw/tweet_emotions.csv")
 
     dataframe = pd.read_csv(data_path)
 
