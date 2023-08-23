63ec50de5eefd5a6858af5d0fda03fa5d590dbcc
SangChu04 <drchasekim@gmail.com>
2023-08-24 08:42:55 +0900

    :white_check_mark: Create test_process_data.py
    test functions inside process_data.py

---
+++ tests/data/test_process_data.py
@@ -0,0 +1,37 @@
+"""
+    src/data/process_data.py 테스트    
+"""
+
+import os
+import sys
+
+import pandas as pd
+
+application_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../")
+sys.path.append(application_path)
+
+
+from src.data.process_data import clean_text
+
+
+def test_clean_text():
+    """
+    test clean_text function
+    """
+    examples = pd.Series(
+        [
+            "@tiffanylue i know  i was listenin to bad habit earlier and i started freakin at his part =[",
+            "worry,Hmmm. http://www.djhero.com/ is down",
+            "Choked on her retainers",
+        ]
+    )
+    answers = pd.Series(
+        [
+            " know was listenin to bad habit earlier and started freakin at his part =[",
+            "worry , hmmm . is down",
+            "choked on her retainers",
+        ]
+    )
+    result = clean_text(examples)
+
+    assert answers.equals(result)
