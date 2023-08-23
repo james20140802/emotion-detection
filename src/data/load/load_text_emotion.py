5938d173a4c9d1b421d5aa97506bcebdf29066a8
SangChu04 <drchasekim@gmail.com>
2023-08-23 16:42:41 +0900

    :art: Change variable name to conform to snake_case

--- src/data/load/load_text_emotion.py
+++ src/data/load/load_text_emotion.py
@@ -14,6 +14,6 @@ def load():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(current_dir, "../../data/raw/Text_Emotion/train.csv")
 
-    df = pd.read_csv(data_path)
+    dataframe = pd.read_csv(data_path)
 
-    return df.loc[:, "text"]
+    return dataframe.loc[:, "text"]
--- src/data/load/load_tweet_emotions.py
+++ src/data/load/load_tweet_emotions.py
@@ -14,6 +14,6 @@ def load():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(current_dir, "../../data/raw/tweet_emotions.csv")
 
-    df = pd.read_csv(data_path)
+    dataframe = pd.read_csv(data_path)
 
-    return df.loc[:, "content"]
+    return dataframe.loc[:, "content"]
