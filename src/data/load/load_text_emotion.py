29f6a65d3045ac549431593e09e6c0d84ee0d7d4
SangChu04 <drchasekim@gmail.com>
2023-08-24 10:49:11 +0900

    :sparkles: Download data from kaggle if not exist

--- src/data/load/load_text_emotion.py
+++ src/data/load/load_text_emotion.py
@@ -5,6 +5,7 @@
 import os
 
 import pandas as pd
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 
 def load():
@@ -13,8 +14,23 @@ def load():
     """
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(
-        current_dir, "../../../data/raw/Text_Emotion/train.csv"
+        current_dir, "../../../data/raw/text-emotion-recognition/train.csv"
     )
+
+    # 만약 local에 데이터가 존재하지 않다면 kaggle에서 다운로드 받음.
+    if not os.path.exists(data_path):
+        api = KaggleApi()
+        api.authenticate()
+
+        download_dir = os.path.join(
+            current_dir, "../../../data/raw/text-emotion-recognition"
+        )
+
+        api.dataset_download_files(
+            "shreejitcheela/text-emotion-recognition",
+            path=download_dir,
+            unzip=True,
+        )
 
     dataframe = pd.read_csv(data_path)
 
--- src/data/load/load_tweet_emotions.py
+++ src/data/load/load_tweet_emotions.py
@@ -5,6 +5,7 @@
 import os
 
 import pandas as pd
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 
 def load():
@@ -13,8 +14,24 @@ def load():
     """
     current_dir = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(
-        current_dir, "../../../data/raw/tweet_emotions.csv"
+        current_dir,
+        "../../../data/raw/emotion-detection-from-text/tweet_emotions.csv",
     )
+
+    # 만약 local에 데이터가 존재하지 않다면 kaggle에서 다운로드 받음.
+    if not os.path.exists(data_path):
+        api = KaggleApi()
+        api.authenticate()
+
+        download_dir = os.path.join(
+            current_dir, "../../../data/raw/emotion-detection-from-text"
+        )
+
+        api.dataset_download_files(
+            "pashupatigupta/emotion-detection-from-text",
+            path=download_dir,
+            unzip=True,
+        )
 
     dataframe = pd.read_csv(data_path)
 
